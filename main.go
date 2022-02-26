a0c40cf4bec34f71014d8d4239c673043efd5a56
Imad Jerra <smartshader@gmail.com>
2022-02-26 18:41:48 -0500

    Support counting bytes

--- main.go
+++ main.go
@@ -5,21 +5,31 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"log"
 	"os"
 )
 
 func main() {
 	lines := flag.Bool("l", false, "Count lines")
+	bytes := flag.Bool("b", false, "Count bytes")
 
 	flag.Parse()
 
-	fmt.Println(count(os.Stdin, *lines))
+	fmt.Println(count(os.Stdin, *lines, *bytes))
 }
 
-func count(r io.Reader, countLines bool) int {
+func count(r io.Reader, countLines bool, countBytes bool) int {
 	scanner := bufio.NewScanner(r)
 
-	if !countLines {
+	if countBytes && countLines {
+		log.Fatalln("Error cannot count both bytes and lines")
+	}
+
+	if countBytes {
+		scanner.Split(bufio.ScanBytes)
+	}
+
+	if !countLines && !countBytes {
 		scanner.Split(bufio.ScanWords)
 	}
 
--- main_test.go
+++ main_test.go
@@ -8,7 +8,7 @@ import (
 func TestCountWords(t *testing.T) {
 	b := bytes.NewBufferString("word1 word2 word3 word4\n")
 	exp := 4
-	res := count(b, false)
+	res := count(b, false, false)
 
 	if res != exp {
 		t.Errorf("Expected %d, got %d instead.\n", exp, res)
@@ -18,7 +18,17 @@ func TestCountWords(t *testing.T) {
 func TestCountLines(t *testing.T) {
 	b := bytes.NewBufferString("word1 word2 word3\nline2\nline3 word1\n")
 	exp := 3
-	res := count(b, true)
+	res := count(b, true, false)
+
+	if res != exp {
+		t.Errorf("Expected %d, got %d instead.\n", exp, res)
+	}
+}
+
+func TestCountBytes(t *testing.T) {
+	b := bytes.NewBufferString("word1 word2 word3 word4\n")
+	exp := 24
+	res := count(b, false, true)
 
 	if res != exp {
 		t.Errorf("Expected %d, got %d instead.\n", exp, res)
